204667a0dd49ac06af65773f979efebcd20fac94
caleb <clfn4y@mst.edu>
2019-05-25 01:40:40 -0500

    working on scroll

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import pygame
 
 import arrow
 
+import subprocess
 
 
 
@@ -83,17 +84,24 @@ class Browse():
 
 
 
-		print ("Title:", search.results[0]['title'])
-		print ("Picture:")
-		print ("Release date:", search.results[0]['release_date'])
-		print ("Description:", search.results[0]['overview'])
-		print ("Genre:", movie.genres[0]['name'], movie.genres[1]['name']) #Will need to check for multiple genres
-		print ("Age:", delta.days)
+		# print ("Title:", search.results[0]['title'])
+		# print ("Picture:")
+		# print ("Release date:", search.results[0]['release_date'])
+		# print ("Description:", search.results[0]['overview'])
+		# print ("Genre:", movie.genres[0]['name'], movie.genres[1]['name']) #Will need to check for multiple genres
+		# print ("Age:", delta.days)
 		
 
-		columns = ['Title', 'Poster', 'Description', 'Release']
+		window.columnconfigure(0, weight=1)
+		window.columnconfigure(3, weight=1)
+		window.columnconfigure(4, weight=1)
+		window.columnconfigure(5, weight=1)
+
+
+
+		columns = ['Title', 'Poster', 'Description', 'Release', 'Genre', 'Play']
 		for c in range(len(columns)):
-			tk.Label(text=columns[c], relief=tk.RIDGE, width=30).grid(row=0,column=c)
+			tk.Label(text=columns[c], relief=tk.RIDGE, width=30, fg='white',bg='black').grid(row=0,column=c)
 
 		title = search.results[0]['title']
 		tk.Label(text=title, relief=tk.RIDGE, width=30, height=20).grid(row=1,column=0)
@@ -131,17 +139,25 @@ class Browse():
 		release = search.results[0]['release_date']
 		tk.Label(text=release, relief=tk.RIDGE, width=30, height=20).grid(row=1,column=3)
 
+
+
+		tk.Label(text=movie.genres[0]['name'], relief=tk.RIDGE, width=30, height=20).grid(row=1,column=4)
+
+
 		self.search_box = Entry(window)
 		self.search_box.insert(0, 'Search')
 		self.search_box.grid(row=2, column=1, sticky=tk.W, pady=4)
 		self.search_box.bind("<FocusIn>", self.clear_widget)
 		tk.Button(window, text='Search', command=self.search_funtction).grid(row=2, column=1, sticky='e', pady=4)
 
-		# word = self.search_box.get()
-		# print (word)
+		
 
-		B = tk.Button(window, text='Play', command=self.Play_movie, bg="blue")
-		B.grid(row=2, column=2)
+		B = tk.Button(window, text='Play', command=self.Play_movie,width=30)
+		B.grid(row=1, column=5, sticky='s',pady=70)
+
+		B = tk.Button(window, text='Open', command=self.Open_movie,width=30)
+		B.grid(row=1, column=5, sticky='n',pady=70)
+
 	def clear_widget(self,event):
 			if self.search_box == window.focus_get() and self.search_box.get() == 'Search':
 					self.search_box.delete(0, tk.END)
@@ -161,7 +177,8 @@ class Browse():
 
 		pygame.quit()
 		# B.configure(background = "green")
-
+	def Open_movie(self):
+		subprocess.Popen(r'explorer /select,"C:\Users\clfur\Documents\Coding Projects\MovieProject\tKinter\MovieDatabase\Divergent.mp4"')
 
 def Update():
 	clear_page()
@@ -217,15 +234,21 @@ if __name__ == "__main__":
 
 # import tkinter as tk
 
-# root = tk.Tk()
-# root.grid_rowconfigure(0, weight=1)
-# root.columnconfigure(0, weight=1)
+root = tk.Tk()
+root.geometry("1000x500")
+root.configure(background='red3')
 
-# frame_main = tk.Frame(root, bg="gray")
-# frame_main.grid(sticky='news')
 
-# label1 = tk.Label(frame_main, text="Label 1", fg="green")
-# label1.grid(row=0, column=0, pady=(5, 0), sticky='nw')
+root.grid_rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+
+
+
+frame_main = tk.Frame(root, bg="red3")
+frame_main.grid(sticky='news')
+
+label1 = tk.Label(frame_main, text="Label 1", fg="green")
+label1.grid(row=0, column=0, pady=(10, 20), sticky='nw')
 
 # label2 = tk.Label(frame_main, text="Label 2", fg="blue")
 # label2.grid(row=1, column=0, pady=(5, 0), sticky='nw')
@@ -233,49 +256,59 @@ if __name__ == "__main__":
 # label3 = tk.Label(frame_main, text="Label 3", fg="red")
 # label3.grid(row=3, column=0, pady=5, sticky='nw')
 
-# # Create a frame for the canvas with non-zero row&column weights
-# frame_canvas = tk.Frame(frame_main)
-# frame_canvas.grid(row=2, column=0, pady=(5, 0), sticky='nw')
-# frame_canvas.grid_rowconfigure(0, weight=1)
-# frame_canvas.grid_columnconfigure(0, weight=1)
-# # Set grid_propagate to False to allow 5-by-5 buttons resizing later
-# frame_canvas.grid_propagate(False)
 
-# # Add a canvas in that frame
-# canvas = tk.Canvas(frame_canvas, bg="yellow")
-# canvas.grid(row=0, column=0, sticky="news")
+frame_main.columnconfigure(0, weight=1)
+frame_main.columnconfigure(3, weight=1)
+frame_main.columnconfigure(4, weight=1)
+frame_main.columnconfigure(5, weight=1)
 
-# # Link a scrollbar to the canvas
-# vsb = tk.Scrollbar(frame_canvas, orient="vertical", command=canvas.yview)
-# vsb.grid(row=0, column=1, sticky='ns')
-# canvas.configure(yscrollcommand=vsb.set)
+columns = ['Title', 'Poster', 'Description', 'Release', 'Genre', 'Play']
+for c in range(len(columns)):
+	tk.Label(frame_main, text=columns[c], relief=tk.RIDGE, width=30).grid(row=1,column=c)
 
-# # Create a frame to contain the buttons
-# frame_buttons = tk.Frame(canvas, bg="blue")
-# canvas.create_window((0, 0), window=frame_buttons, anchor='nw')
+# Create a frame for the canvas with non-zero row&column weights
+frame_canvas = tk.Frame(frame_main)
+frame_canvas.grid(row=2, column=0, sticky='nw')
+frame_canvas.grid_rowconfigure(0, weight=1)
+frame_canvas.grid_columnconfigure(0, weight=1)
+# Set grid_propagate to False to allow 5-by-5 buttons resizing later
+frame_canvas.grid_propagate(False)
 
-# # Add 9-by-5 buttons to the frame
-# rows = 9
-# columns = 5
-# buttons = [[tk.Button() for j in range(columns)] for i in range(rows)]
-# for i in range(0, rows):
-# 		for j in range(0, columns):
-# 				buttons[i][j] = tk.Button(frame_buttons, text=("%d,%d" % (i+1, j+1)))
-# 				buttons[i][j].grid(row=i, column=j, sticky='news')
+# Add a canvas in that frame
+canvas = tk.Canvas(frame_canvas, bg="yellow")
+canvas.grid(row=0, column=0, sticky="news")
 
-# # Update buttons frames idle tasks to let tkinter calculate buttons sizes
-# frame_buttons.update_idletasks()
+# Link a scrollbar to the canvas
+vsb = tk.Scrollbar(frame_canvas, orient="vertical", command=canvas.yview)
+vsb.grid(row=0, column=1, sticky='ns')
+canvas.configure(yscrollcommand=vsb.set)
 
-# # Resize the canvas frame to show exactly 5-by-5 buttons and the scrollbar
-# first5columns_width = sum([buttons[0][j].winfo_width() for j in range(0, 5)])
-# first5rows_height = sum([buttons[i][0].winfo_height() for i in range(0, 5)])
-# frame_canvas.config(width=first5columns_width + vsb.winfo_width(),
-# 										height=first5rows_height)
+# Create a frame to contain the buttons
+frame_buttons = tk.Frame(canvas, bg="blue")
+canvas.create_window((0, 0), window=frame_buttons, anchor='nw')
 
-# # Set the canvas scrolling region
-# canvas.config(scrollregion=canvas.bbox("all"))
+# Add 9-by-5 buttons to the frame
+rows = 9
+columns = 5
+buttons = [[tk.Button() for j in range(columns)] for i in range(rows)]
+for i in range(0, rows):
+		for j in range(0, columns):
+				buttons[i][j] = tk.Button(frame_buttons, text=("%d,%d" % (i+1, j+1)))
+				buttons[i][j].grid(row=i, column=j, sticky='news')
 
-# # Launch the GUI
-# root.mainloop()
+# Update buttons frames idle tasks to let tkinter calculate buttons sizes
+frame_buttons.update_idletasks()
+
+# Resize the canvas frame to show exactly 5-by-5 buttons and the scrollbar
+first5columns_width = sum([buttons[0][j].winfo_width() for j in range(0, 5)])
+first5rows_height = sum([buttons[i][0].winfo_height() for i in range(0, 5)])
+frame_canvas.config(width=first5columns_width + vsb.winfo_width(),
+										height=first5rows_height)
+
+# Set the canvas scrolling region
+canvas.config(scrollregion=canvas.bbox("all"))
+
+# Launch the GUI
+root.mainloop()
 
 
